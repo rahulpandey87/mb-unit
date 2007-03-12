5edbdb9d0fb4203724040382be593cf3e4da4a69
astopford <astopford@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-03-12 16:01:25 +0000

    Private member tests contribution from Vadim.

--- trunk/v2a/Releases/2.4/mbunit/MbUnit.Framework.2.0/Reflection/ReflectorInstance.cs
+++ trunk/v2a/Releases/2.4/mbunit/MbUnit.Framework.2.0/Reflection/ReflectorInstance.cs
@@ -5,9 +5,11 @@ using System.Reflection;
 
 namespace MbUnit.Framework.Reflection
 {
+    public enum MemberType { Method, Field, Property }
+
     public partial class Reflector
     {
-        object obj;
+        object _obj;
 
         /// <summary>
         /// Constructor for object
@@ -15,7 +17,9 @@ namespace MbUnit.Framework.Reflection
         /// <param name="obj">Object to be referred to in methods</param>
         public Reflector(object obj)
         {
-            this.obj = obj;
+            if (obj == null)
+                throw new NullReferenceException("obj cannot be null.");
+            _obj = obj;
         }
 
         /// <summary>
@@ -26,14 +30,43 @@ namespace MbUnit.Framework.Reflection
         /// <returns></returns>
         public object RunPrivateMethod(string methodName)
         {
-            Type objType = obj.GetType();
+            return RunPrivateMethod(methodName, null);
+        }
 
-            MethodInfo methodRequired = objType.GetMethod(methodName, BindingFlags.NonPublic | 
-                                                                        BindingFlags.Instance | 
-                                                                        BindingFlags.Public | 
-                                                                        BindingFlags.Static);
+        public object RunPrivateMethod(string methodName, params object[] methodParams)
+        {
+            Type[] paramTypes = null;
 
-            return methodRequired.Invoke(obj, null);
+            if (methodParams != null)
+            {
+                paramTypes = new Type[methodParams.Length];
+
+                for (int ndx = 0; ndx < methodParams.Length; ndx++)
+                    paramTypes[ndx] = methodParams[ndx].GetType();
+            }
+            else
+                paramTypes = new Type[0];
+
+            MethodInfo mi = _obj.GetType().GetMethod(methodName
+                    , BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance
+                    , null, paramTypes, null);
+
+            IsMember(mi, methodName, MemberType.Method);
+            return mi.Invoke(_obj, methodParams);
+        }
+
+        public object GetNonPublicField(string fieldName)
+        {
+            FieldInfo fi = _obj.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            IsMember(fi, fieldName, MemberType.Field);
+            return fi.GetValue(_obj);
+        }
+
+        public object GetNonPublicProperty(string propName)
+        {
+            PropertyInfo pi = _obj.GetType().GetProperty(propName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            IsMember(pi, propName, MemberType.Property);
+            return pi.GetValue(_obj, null);
         }
 
         /// <summary>
@@ -42,16 +75,23 @@ namespace MbUnit.Framework.Reflection
         /// <param name="obj">Object which contains field</param>
         /// <param name="variableName">Field Name</param>
         /// <returns></returns>
+        [Obsolete("Use GetNonPublicField instead")]
         public object GetPrivateVariable(string variableName)
         {
-            Type objType = obj.GetType();
+            Type objType = _obj.GetType();
 
             FieldInfo variableInfo = objType.GetField(variableName, BindingFlags.NonPublic |
                                                                         BindingFlags.Instance |
                                                                         BindingFlags.Public |
                                                                         BindingFlags.Static);
 
-            return variableInfo.GetValue(obj);
+            return variableInfo.GetValue(_obj);
+        }
+
+        private void IsMember(object member, string memberName, MemberType type)
+        {
+            if (member == null)
+                throw new ReflectionException(memberName, type, _obj);
         }
     }
 }
