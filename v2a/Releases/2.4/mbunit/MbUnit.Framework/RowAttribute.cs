f1754d000b7c82e2d6ec6b49c602d6c4bf22a850
vkreynin <vkreynin@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-05-15 16:43:17 +0000

    Julian changes for RowTest attribute.

--- v2a/Releases/2.4/mbunit/MbUnit.Framework/RowAttribute.cs
+++ v2a/Releases/2.4/mbunit/MbUnit.Framework/RowAttribute.cs
@@ -1,9 +1,19 @@
 ﻿using System;
 using MbUnit.Core.Framework;
-using System.Collections;
+using System.Reflection;
+using MbUnit.Core;
+using System.Globalization;
 
 namespace MbUnit.Framework
 {
+    public enum SpecialValue
+    {
+        /// <summary>
+        /// When used as parameter in a row test, it will be replaced by null (Nothing in VB).
+        /// </summary>
+        Null
+    }
+
     /// <summary>
     /// Provides a row of values using in conjunction with <see cref="RowTestAttribute" />
     /// to bind values to the parameters of a row test method.
@@ -60,15 +70,80 @@ namespace MbUnit.Framework
         }
 
         /// <summary>
-        /// Gets the row of values.  If there're any decimal parameters, keep them decimal.
+        /// Gets the row of values. Each one will be converted (if posible) to the type of
+        /// the corresponding argument in the test method.
         /// </summary>
-        /// <param name="decimalParamIndexes">Index list of decimal parameters.</param>
-        /// <returns>The row of values</returns>
-        public Object[] GetRow(ArrayList decimalParamIndexes)
+        /// <param name="parameters">List of parameters.</param>
+        /// <returns>The row of values.</returns>
+        public Object[] GetRow(ParameterInfo[] parameters)
         {
-            for (int ndx = 0; ndx < decimalParamIndexes.Count; ndx++)
-                row[ndx] = Convert.ToDecimal(row[ndx]);
-            return GetRow();
+            // If the lenghts are different a TargetParameterCountException exception will be thrown
+            if (row.Length == parameters.Length)
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    // Skip nulls
+                    if (row[i] == null) continue;
+                    // If the current item in the row is a Type and the test method is expecting
+                    // an instance, then create one
+                    if (row[i] == parameters[i].ParameterType)
+                    {
+                        if (!parameters[i].ParameterType.IsEnum)
+                        {
+                            row[i] = TypeHelper.CreateInstance(parameters[i].ParameterType);
+                        }
+                    }
+                     //If the item is SpecialValue.Null then return null
+                    else if (row[i].GetType() == typeof(SpecialValue))
+                    //if (row[i].GetType() == typeof(SpecialValue))
+                    {
+                        row[i] = GetSpecialValue((SpecialValue)row[i]);
+                    }
+                    // Try to convert the type to the one expected by the test method
+                    else if ((row[i] as IConvertible) != null)
+                    {
+                        IFormatProvider formatProvider = GetFormatProvider(parameters[i].ParameterType);
+                        row[i] = Convert.ChangeType(row[i], parameters[i].ParameterType, formatProvider);
+                    }
+                }
+            }
+
+            return this.row;
         }
+
+        private object GetSpecialValue(SpecialValue sv)
+        {
+            if (sv == SpecialValue.Null)
+            {
+                return null;
+            }
+
+            return null;
+        }
+
+        private IFormatProvider GetFormatProvider(Type t)
+        {
+            IFormatProvider formatProvider = null;
+            if (IsNumericType(t))
+            {
+                formatProvider = NumberFormatInfo.InvariantInfo;
+            }
+            else if (t == typeof(DateTime))
+            {
+                formatProvider = DateTimeFormatInfo.InvariantInfo;
+            }
+            return formatProvider;
+        }
+
+        private bool IsNumericType(Type t)
+        {
+            return t == typeof(double) ||
+                   t == typeof(float) ||
+                   t == typeof(decimal) ||
+                   t == typeof(int) ||
+                   t == typeof(long) ||
+                   t == typeof(short);
+        }
+
     }
 }
