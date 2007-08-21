5ea352d4604e72536f18276058d03446184b8425
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-08-21 15:14:04 +0000

    Changes to correct version info (MBUNIT-130).

---
+++ v2a/Releases/2.4.1/Tests/MbUnit.Framework/AssemblyInfoAdditional.cs
@@ -0,0 +1,9 @@
+#region Using directives
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+[assembly: AssemblyTitle("MbUnit Framework Test Suite")]
+[assembly: AssemblyDescription("A test suite for the MbUnit Framework.")]
