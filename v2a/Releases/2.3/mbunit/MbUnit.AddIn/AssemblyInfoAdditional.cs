1a873f945859a418e8d77ffa1e4b8c7a3f874f81
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-11-22 00:19:32 +0000

    Move v2.4 branch.

--- v2a/Releases/2.3/mbunit/MbUnit.AddIn/AssemblyInfoAdditional.cs
+++
@@ -1,8 +0,0 @@
-#region Using directives
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-#endregion
-
-[assembly: AssemblyTitle("MbUnit.AddIn 2.3.1040")]
--- v2a/Releases/2.4.1/mbunit/MbUnit.MSBuild.Tasks/AssemblyInfoAdditional.cs
+++
@@ -1,8 +0,0 @@
-#region Using directives
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-#endregion
-
-[assembly: AssemblyTitle("MbUnit.MSBuild.Tasks 2.4.1040")]
