538a6d40d66da493485cd867a12bc83a7209867c
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-12-29 09:34:47 +0000

    More MbUnit v2 build fixes.

--- trunk/v2/src/mbunit/MbUnit.Demo.1.1/AssemblyInfoAdditional.cs
+++
@@ -1,9 +0,0 @@
-#region Using directives
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-#endregion
-
-[assembly: AssemblyTitle("MbUnit Demo 1.1")]
-[assembly: AssemblyDescription("MbUnit demonstration project (.NET 1.1).")]
