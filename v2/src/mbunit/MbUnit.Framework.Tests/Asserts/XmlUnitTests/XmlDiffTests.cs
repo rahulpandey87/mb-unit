aa067a9bc27184e99be9c6c112b48fdc2323e446
vkreynin <vkreynin@06ccd1c7-0227-0410-afd2-95788076aca3>
2007-12-31 03:18:46 +0000

    1. Fixed NotEqualAssertionException test. 2. Added more tests for XmlEquals. 3. Added some tests to test the difference between DiffResults.Equal & DiffResult.Identical. 4. Modified & Refactor XmlEquals method to display Optional Description if any.

--- v2/src/mbunit/MbUnit.Framework.Tests/Asserts/XmlUnitTests/XmlDiffTests.cs
+++ v2/src/mbunit/MbUnit.Framework.Tests/Asserts/XmlUnitTests/XmlDiffTests.cs
@@ -1,10 +1,12 @@
-namespace MbUnit.Framework.Tests.Asserts.XmlUnit {
+namespace MbUnit.Framework.Tests.Asserts.XmlUnit
+{
     using MbUnit.Framework;
     using MbUnit.Framework.Xml;
-	using System.IO;
-    
+    using System.IO;
+
     [TestFixture]
-    public class XmlDiffTests {
+    public class XmlDiffTests
+    {
         private XmlDiff _xmlDiff;
 
         [Test]
@@ -27,11 +29,28 @@ namespace MbUnit.Framework.Tests.Asserts.XmlUnit {
 
         private void AssertExpectedResult(string input1, string input2, bool expected)
         {
+            AssertExpectedEqualResult(input1, input2, expected);
+        }
+
+        private void AssertExpectedEqualResult(string input1, string input2, bool expected)
+        {
+            DiffResult result = GetDiffResult(input1, input2);
+            string msg = "Equal: comparing " + input1 + " to " + input2 + ": " + result.Difference;
+            Assert.AreEqual(expected, GetDiffResult(input1, input2).Equal, msg);
+        }
+
+        private void AssertExpectedIdenticalResult(string input1, string input2, bool expected)
+        {
+            DiffResult result = GetDiffResult(input1, input2);
+            string msg = "Identical: comparing " + input1 + " to " + input2 + ": " + result.Difference;
+            Assert.AreEqual(expected, result.Identical, msg);
+        }
+
+        private DiffResult GetDiffResult(string input1, string input2)
+        {
             TextReader reader1 = new StringReader(input1);
             TextReader reader2 = new StringReader(input2);
-            DiffResult result = PerformDiff(reader1, reader2);
-            string msg = "comparing " + input1 + " to " + input2 + ": " + result.Difference;
-            Assert.AreEqual(expected, result.Equal, msg);
+            return PerformDiff(reader1, reader2);
         }
 
         private void AssertExpectedResult(string[] inputs1, string[] inputs2, bool expected)
@@ -61,7 +80,7 @@ namespace MbUnit.Framework.Tests.Asserts.XmlUnit {
             AssertExpectedResult(input1, input2, true);
         }
 
-       
+
         //[Test]
         //public void NotEqualResultForEmptyVsNotEmptyElements()
         //{
@@ -69,47 +88,69 @@ namespace MbUnit.Framework.Tests.Asserts.XmlUnit {
         //    string[] input2 = { "<empty>text</empty>", "<empty>text</empty>", "<empty>text</empty>" };
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
         //[Test] public void NotEqualResultForDifferentElements() { 
         //    string[] input1 = {"<a><b/></a>" , "<a><b/></a>", "<a><b/></a>"};
         //    string[] input2 = {"<b><a/></b>", "<a><c/></a>", "<a><b><c/></b></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
         //[Test] public void NotEqualResultForDifferentNumberOfAttributes() { 
         //    string[] input1 = {"<a><b x=\"1\"/></a>", "<a><b x=\"1\"/></a>"};
         //    string[] input2 = {"<a><b/></a>", "<a><b x=\"1\" y=\"2\"/></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
         //[Test] public void NotEqualResultForDifferentAttributeValues() { 
         //    string[] input1 = {"<a><b x=\"1\"/></a>", "<a><b x=\"1\" y=\"2\"/></a>"};
         //    string[] input2 = {"<a><b x=\"2\"/></a>", "<a><b x=\"1\" y=\"3\"/></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
         //[Test] public void NotEqualResultForDifferentAttributeNames() { 
         //    string[] input1 = {"<a><b x=\"1\"/></a>", "<a><b x=\"1\" y=\"2\"/></a>"};
         //    string[] input2 = {"<a><b y=\"2\"/></a>", "<a><b x=\"1\" z=\"3\"/></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
-        //[Test] public void EqualResultForDifferentAttributeSequences() { 
-        //    string[] input1 = {"<a x=\"1\" y=\"2\" z=\"3\"/>", 
-        //                        "<a><b x=\"1\" y=\"2\"/></a>"};
-        //    string[] input2 = {"<a y=\"2\" z=\"3\" x=\"1\"/>", 
-        //                        "<a><b y=\"2\" x=\"1\"/></a>"};
-        //    AssertExpectedResult(input1, input2, true);
-        //}
-        
-        //[Test] public void NotEqualResultForDifferentAttributeValuesAndSequences() { 
-        //    string[] input1 = {"<a x=\"1\" y=\"2\" z=\"3\"/>", 
-        //                        "<a><b x=\"1\" y=\"2\"/></a>"};
-        //    string[] input2 = {"<a y=\"2\" z=\"3\" x=\"2\"/>", 
-        //                        "<a><b y=\"1\" x=\"1\"/></a>"};
-        //    AssertExpectedResult(input1, input2, false);
-        //}
-        
+
+        [Test]
+        public void EqualResultForDifferentAttributeSequences()
+        {
+            string[] input1 = {"<a x=\"1\" y=\"2\" z=\"3\"/>", 
+                                "<a><b x=\"1\" y=\"2\"/></a>"};
+            string[] input2 = {"<a y=\"2\" z=\"3\" x=\"1\"/>", 
+                                "<a><b y=\"2\" x=\"1\"/></a>"};
+            AssertExpectedResult(input1, input2, true);
+        }
+
+        [Test]
+        public void NotEqualResultForDifferentAttributeValuesAndSequences()
+        {
+            string[] input1 = {"<a x=\"1\" y=\"2\" z=\"3\"/>", 
+                                "<a><b x=\"1\" y=\"2\"/></a>"};
+            string[] input2 = {"<a y=\"2\" z=\"3\" x=\"2\"/>", 
+                                "<a><b y=\"1\" x=\"1\"/></a>"};
+            AssertExpectedResult(input1, input2, false);
+        }
+
+        [Test]
+        public void NotEqualResultForDifferentAttributeValuesAndSequences2()
+        {
+            string input1 = "<a x=\"1\" y=\"2\"/>";
+            string input2 = "<a y=\"2\" x=\"1\"/>";
+            AssertExpectedEqualResult(input1, input2, true);
+            AssertExpectedIdenticalResult(input1, input2, false);
+        }
+
+        [Test]
+        public void NotEqualResultWhenHasXmlDeclarationID()
+        {
+            string input1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><a/>";
+            string input2 = "<a/>";
+            AssertExpectedEqualResult(input1, input2, true);
+            AssertExpectedIdenticalResult(input1, input2, false);
+        }
+
         //[Test] public void NotEqualResultForDifferentTextElements() {
         //    string[] input1 = {"<a>text</a>", "<a>text<b>more text</b></a>", 
         //                        "<a><b>text</b>more text</a>"};
@@ -117,19 +158,19 @@ namespace MbUnit.Framework.Tests.Asserts.XmlUnit {
         //                        "<a>more text<b>text</b></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
         //[Test] public void CanDistinguishElementClosureAndEmptyElement() {
         //    string[] input1 = {"<a><b>text</b></a>", "<a>text<b>more text</b></a>"};
         //    string[] input2 = {"<a><b/>text</a>", "<a>text<b/>more text</a>"};
         //    AssertExpectedResult(input1, input2, false);
-            
+
         //}
-        
+
         //[Test] public void NotEqualResultForDifferentLengthElements() {
         //    string[] input1 = {"<a>text</a>", "<a><b>text</b><c>more text</c></a>"};
         //    string[] input2 = {"<a>text<b/></a>", "<a><b>text</b>more text<c/></a>"};
         //    AssertExpectedResult(input1, input2, false);
         //}
-        
+
     }
 }
