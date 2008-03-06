cd2c754eca6972dca13cef3d4677d0be80c1147d
grahamrhay <grahamrhay@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-03-06 15:29:49 +0000

    Fixed a bug (or two) in ExtractResourceAttribute and uncommented tests.

--- trunk/v2/src/mbunit/MbUnit.Framework.Tests/Core/ExtractResourceTest.cs
+++ trunk/v2/src/mbunit/MbUnit.Framework.Tests/Core/ExtractResourceTest.cs
@@ -1,32 +1,29 @@
 using System;
 using System.ComponentModel;
 using System.IO;
+
 using MbUnit.Framework;
 
-namespace MbUnit.Tests.Core.Framework
+namespace MbUnit.Framework.Tests.Attributes.Decorators
 {
 	[TestFixture]
 	public class ExtractResourceTests
 	{
-        //[Test]
-        //[Category("Attributes")]
-        //[ExtractResource("EmbeddedResourceSample.txt")]
-        //public void CanExtractResource()
-        //{
-        //    Assert.IsTrue(File.Exists("EmbeddedResourceSample.txt"), "Could not find the extracted resource file");
-        //}
+        [Test, Category("Attributes.Decorators"), ExtractResource("MbUnit.Framework.Tests.Core.EmbeddedResourceSample.txt", "EmbeddedResourceSample.txt")]
+        public void CanExtractResource()
+        {
+            Assert.IsTrue(File.Exists("EmbeddedResourceSample.txt"), "Could not find the extracted resource file");
+        }
 
-        //[Test]
-        //[Category("Attributes")]
-        //[ExtractResource("EmbeddedResourceSample.txt")]
-        //public void CanExtractResourceToStream()
-        //{
-        //    Stream stream = ExtractResourceAttribute.Stream;
-        //    Assert.IsNotNull(stream, "The Stream is null");
-        //    using (StreamReader reader = new StreamReader(stream))
-        //    {
-        //        Assert.AreEqual("Hello World! The ExtractResourceAttribute works!", reader.ReadToEnd(), "The contents of the resource is not what we expected.");
-        //    }
-        //}	
+        [Test, Category("Attributes.Decorators"), ExtractResource("MbUnit.Framework.Tests.Core.EmbeddedResourceSample.txt")]
+        public void CanExtractResourceToStream()
+        {
+            Stream stream = ExtractResourceAttribute.Stream;
+            Assert.IsNotNull(stream, "The Stream is null");
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Assert.AreEqual("Hello World! The ExtractResourceAttribute works!", reader.ReadToEnd(), "The contents of the resource is not what we expected.");
+            }
+        }
 	}
 }
