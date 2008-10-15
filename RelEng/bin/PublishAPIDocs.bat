c6b1bb2b49ce87f8409285ec1e401a9be1dcef28
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-10-15 19:09:43 +0000

    Added some scripts to publish the book and API docs.

---
+++ RelEng/bin/PublishAPIDocs.bat
@@ -0,0 +1,41 @@
+@if defined ECHO (echo %ECHO%) else (echo off)
+REM
+REM Publishes the specified Book folder to the web site.
+REM
+setlocal
+set ROBOCOPY=%~dp0robocopy.exe
+set TARGET=C:\Inetpub\www.gallio.org\gallio.org\api
+
+set VERSION=%~1
+if not defined VERSION (
+    echo Usage: [version]
+    exit /b 1
+)
+
+set SOURCE=C:\RelEng\Distributables\APIDocs\%VERSION%
+
+echo.
+echo Version: %VERSION%
+echo Source : %SOURCE%
+echo Target : %TARGET%
+echo.
+
+call :CHECKFILE Gallio.chm
+if errorlevel 1 goto :ERROR
+call :CHECKFILE html\index.html
+if errorlevel 1 goto :ERROR
+
+robocopy "%SOURCE%" "%TARGET%" /MIR
+
+:ERROR
+exit /b %ERRORLEVEL%
+
+
+:CHECKFILE
+if not exist "%SOURCE%\%~1" (
+    echo Missing %~1!  Source directory may be incorrect.
+    exit /b 1
+)
+
+exit /b 0
+
