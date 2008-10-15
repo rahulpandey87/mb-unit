ad3c1a9e40b5f693827ad86f4c93c2162ba6bd81
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2008-10-15 02:37:50 +0000

    Updated docs page to point to book and API docs.

--- trunk/Website/www.gallio.org/gallio.org/Docs.aspx.cs
+++ trunk/Website/www.gallio.org/gallio.org/Docs.aspx.cs
@@ -9,7 +9,7 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 
-public partial class DocsWait : System.Web.UI.Page
+public partial class Docs : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
