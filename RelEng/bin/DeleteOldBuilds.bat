0dc9b9d37a33bdbd260a31564d70e9478b7dbbf2
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2009-03-30 07:38:38 +0000

    Create v3.0.6 branch.

--- RelEng/bin/DeleteOldBuilds.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Purges files from the distributables folder that have outlived
-REM their usefulness.
-
-c:\cygwin\bin\find c:\RelEng\Distributables -daystart -maxdepth 1 -mindepth 1 -type f -mtime +7 -delete
