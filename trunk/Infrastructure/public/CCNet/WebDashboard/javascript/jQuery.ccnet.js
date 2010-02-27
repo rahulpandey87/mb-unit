ef76ee29eb7306d6554ee1d4c7a4652a70d30d6f
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-02-27 04:28:40 +0000

    Adding CCNet 1.5.

---
+++ trunk/Infrastructure/public/CCNet/WebDashboard/javascript/jQuery.ccnet.js
@@ -0,0 +1,2 @@
+
+(function($){$.tablesorter.addWidget({id:'statusDisplay',format:function(table){$("tr.buildStatus",table.tBodies[0]).each(function(){var row=$(this);var id=row.attr('id');var linkedId='projectData'+id.substring(4);var parent=$('#'+linkedId);row.insertAfter(parent);});}});$.fn.initialiseProjectGrid=function(config){var defaultConfig={sortList:[[0,0]]};config=$.extend(defaultConfig,config||{});this.tablesorter({sortList:config.sortList,widgets:['statusDisplay']});return this;};})(jQuery);
