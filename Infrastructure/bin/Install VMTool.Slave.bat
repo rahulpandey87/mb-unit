4b5c0e7a711ccff51c96cc2104228cc275474abe
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2010-02-28 12:02:55 +0000

    Add a reminder to scripts to change username.

--- Infrastructure/bin/Install VMTool.Slave.bat
+++ Infrastructure/bin/Install VMTool.Slave.bat
@@ -1,2 +1,5 @@
 @echo off
 sc create VMTool.Slave binPath= "%~dp0VMTool.Slave.exe" start= auto DisplayName= "VMTool Slave"
+echo.
+echo Remember to change service account to user "Infrastructure".
+echo.
