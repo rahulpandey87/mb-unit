2ef5702d0b1d0a20713b54498dfabc5d2a5ba278
jeff.brown <jeff.brown@06ccd1c7-0227-0410-afd2-95788076aca3>
2011-02-14 18:16:20 +0000

    Update build scripts.

--- Infrastructure/bin/DeleteOldBuildsInternal.bat
+++ Infrastructure/bin/DeleteOldBuildsInternal.bat
@@ -6,5 +6,7 @@ c:\cygwin\bin\find /cygdrive/d/Distributables -daystart -maxdepth 1 -mindepth 1 
 c:\cygwin\bin\find /cygdrive/d/Distributables/APIDocs -daystart -maxdepth 1 -mindepth 1 -type d -mtime +14 -execdir c:\cygwin\bin\rm -rf "{}" ";"
 c:\cygwin\bin\find /cygdrive/d/Distributables/LibCheck -daystart -maxdepth 1 -mindepth 1 -type d -mtime +14 -execdir c:\cygwin\bin\rm -rf "{}" ";"
 c:\cygwin\bin\find /cygdrive/d/Distributables/GallioBook -daystart -maxdepth 1 -mindepth 1 -type d -mtime +14 -execdir c:\cygwin\bin\rm -rf "{}" ";"
+c:\cygwin\bin\find /cygdrive/d/Distributables/Symbols -daystart -maxdepth 1 -mindepth 1 -type d -mtime +14 -execdir c:\cygwin\bin\rm -rf "{}" ";"
+c:\cygwin\bin\find /cygdrive/d/Distributables/WebSites -daystart -maxdepth 1 -mindepth 1 -type d -mtime +14 -execdir c:\cygwin\bin\rm -rf "{}" ";"
 
 exit /b 0
